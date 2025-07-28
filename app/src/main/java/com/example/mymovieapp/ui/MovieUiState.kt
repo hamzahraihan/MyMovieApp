61de9c6171c372902ae33a6a9935aa8f3a5224f6
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2025-07-28 19:54:30 +0700

    create MovieUiState.kt for view model

---
+++ app/src/main/java/com/example/mymovieapp/ui/MovieUiState.kt
@@ -0,0 +1,12 @@
+package com.example.mymovieapp.ui
+
+import com.example.mymovieapp.data.Movie
+import com.example.mymovieapp.data.MovieType
+import com.example.mymovieapp.data.local.LocalMovieDataProvider
+
+data class MovieUiState(
+    val movies: Map<MovieType, List<Movie>> = emptyMap(),
+    val currentMovieType: MovieType = MovieType.POPULAR,
+    val currentSelectedMovie: Movie = LocalMovieDataProvider.defaultValue,
+    val isShowingHomepage: Boolean = true
+)
