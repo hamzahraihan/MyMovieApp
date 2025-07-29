cc577f45ab480a8806188303767696f0d42311e4
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2025-07-29 19:25:14 +0700

    change from scaffold to surface and add windowSize parameter to MovieApp.kt

--- app/src/main/java/com/example/mymovieapp/MainActivity.kt
+++ app/src/main/java/com/example/mymovieapp/MainActivity.kt
@@ -5,28 +5,28 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material3.Surface
+import androidx.compose.material3.windowsizeclass.ExperimentalMaterial3WindowSizeClassApi
+import androidx.compose.material3.windowsizeclass.calculateWindowSizeClass
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalLayoutDirection
-import androidx.compose.ui.tooling.preview.Preview
+import com.example.mymovieapp.ui.MovieApp
 import com.example.mymovieapp.ui.MovieHomeScreen
 import com.example.mymovieapp.ui.theme.MyMovieAppTheme
 
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3WindowSizeClassApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             MyMovieAppTheme {
-                Scaffold(
+                Surface(
                     modifier = Modifier.fillMaxSize(),
-                    containerColor = MaterialTheme.colorScheme.surfaceContainer
-                ) { innerPadding ->
-                    MovieHomeScreen(contentPadding = innerPadding)
+                ) {
+                    val windowSize = calculateWindowSizeClass(this)
+                    MovieApp(windowSize = windowSize.widthSizeClass)
                 }
             }
         }
