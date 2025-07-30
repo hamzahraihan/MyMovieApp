e68b55891508278f70ad44d4a6eac84e28d9913a
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2025-07-30 16:28:15 +0700

    add bodyMedium typography

--- app/src/main/java/com/example/mymovieapp/ui/theme/Type.kt
+++ app/src/main/java/com/example/mymovieapp/ui/theme/Type.kt
@@ -35,6 +35,11 @@ val Typography = Typography(
         lineHeight = 24.sp,
         letterSpacing = 0.5.sp
     ),
+    bodyMedium = TextStyle(
+        fontFamily = Inter,
+        fontWeight = FontWeight.Normal,
+        fontSize = 14.sp,
+    ),
     bodySmall = TextStyle(
         fontFamily = Inter,
         fontWeight = FontWeight.Normal,
